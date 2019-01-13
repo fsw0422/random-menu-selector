60999d8aea8f96cbb877b67d6b046ed6b447b946
Kevin Kwon <fsw0422@gmail.com>
2019-01-13 02:35:01 +0100

    Working

---
+++ public/javascripts/index.js
@@ -0,0 +1,16 @@
+$(function() {
+  //$(".input").keypress(function (e) {
+  //  // User press `Enter` key in input box
+  //  if (e.which === 13) {
+  //    updatePageLocation();
+  //    return false;
+  //  }
+  //});
+
+  //function createOrUpdateMenu(query, sortBy, onSuccess) {
+  //  var endpoint = "/menu";
+  //  $.post(endpoint, onSuccess).fail(function (response) {
+  //      $("#infobox-content").html(response.responseText);
+  //  });
+  //}
+});
