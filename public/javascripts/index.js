2abc4b090900c7e1ecef3565e010e8c9968e51db
Kevin Kwon <fsw0422@gmail.com>
2019-01-21 01:23:06 +0100

    working

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,36 @@
 $(function() {
 
-  $(".test").click(function() {
-    $.get("/menu/view", function(data, status) {
-      console.log(JSON.stringify(data));
+  $('#search').click(function() {
+    $.ajax({
+        url: '/menu/view',
+        type: 'get',
+    })
+    .done(function(response) {
+      console.log(JSON.stringify(response));
+    })
+    .fail(function(jqXHR, textStatus) {
+      console.log(textStatus);
     });
   });
+
+  $('#submit').click(function() {
+    var name = $("#name").val();
+    var password = $("#password").val();
+    $.ajax({
+        url: '/menu',
+        type: 'post',
+        contentType: 'application/json',
+        data: JSON.stringify({
+          'name': name,
+          'password': password
+        })
+    })
+    .done(function(response) {
+      console.log(JSON.stringify(response));
+    })
+    .fail(function(jqXHR, textStatus) {
+      console.log(textStatus);
+    });
+  });
+
 });
