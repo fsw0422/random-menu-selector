6a7520edb302d5193d2fef2f3822785467e0bd5b
Kevin Kwon <fsw0422@gmail.com>
2019-03-12 22:22:12 +0100

    Refactored test

--- test/menu/CommandControllerTest.scala
+++ test/menu/CommandControllerTest.scala
@@ -5,8 +5,8 @@ import java.util.UUID
 import event.EventDao
 import org.junit.runner.RunWith
 import org.mockito.{ArgumentMatchersSugar, MockitoSugar}
-import org.scalatest.FunSpec
 import org.scalatest.junit.JUnitRunner
+import org.scalatest.{FlatSpec, GivenWhenThen, Matchers}
 import play.api.inject.bind
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.json.Json
@@ -20,9 +20,11 @@ import scala.concurrent.Future
 
 @RunWith(classOf[JUnitRunner])
 class CommandControllerTest
-    extends FunSpec
+    extends FlatSpec
     with MockitoSugar
     with ArgumentMatchersSugar
+    with GivenWhenThen
+    with Matchers
     with Results {
   private val emailSenderMock = mock[EmailSender]
   private val eventDaoMock = mock[EventDao]
@@ -39,59 +41,56 @@ class CommandControllerTest
     .build
   private implicit val dispatcher = mockedApp.actorSystem.dispatcher
 
-  describe("""
-       GIVEN 2 menus
-         AND an user
-       WHEN POST request to /menu/random
-  """) {
-    it("SHOULD return ok status with either menu's UUID") {
-      val applePieUuid = UUID
-        .fromString("123e4567-e89b-12d3-a456-426655440000")
-      val applePieView = MenuView(
-        Some(applePieUuid),
-        "ApplePie",
-        Seq("apple", "pie"),
-        "bake apple",
-        "appleLink"
-      )
+  behavior of "POST request to /menu/random endpoint"
 
-      val pearPieUuid = UUID
-        .fromString("223e4567-e89b-12d3-a456-426655440000")
-      val pearPieView = MenuView(
-        Some(pearPieUuid),
-        "PearPie",
-        Seq("pear", "pie"),
-        "boil pear",
-        "pearLink"
-      )
-      when(menuViewDaoMock.findAll())
+  it should "return ok status with random menu's UUID" in {
+    Given("an apple pie and pear pie")
+    val applePieUuid = UUID
+      .fromString("123e4567-e89b-12d3-a456-426655440000")
+    val applePieView = MenuView(
+      Some(applePieUuid),
+      "ApplePie",
+      Seq("apple", "pie"),
+      "bake apple",
+      "appleLink"
+    )
+    val pearPieUuid = UUID
+      .fromString("223e4567-e89b-12d3-a456-426655440000")
+    val pearPieView = MenuView(
+      Some(pearPieUuid),
+      "PearPie",
+      Seq("pear", "pie"),
+      "boil pear",
+      "pearLink"
+    )
+    when(menuViewDaoMock.findAll())
       .thenReturn(Future(Seq(applePieView, pearPieView)))
-      when(menuViewDaoMock.findByName(any[String]))
+    when(menuViewDaoMock.findByName(any[String]))
       .thenReturn(Future(Seq(applePieView)))
 
-      val userUuid = UUID
+    And("a default user James")
+    val userUuid = UUID
       .fromString("124e4567-e89b-12d3-a456-426655440000")
-      val userView = UserView(Some(userUuid), "james", "james@email.com")
-      when(userViewDaoMock.findAll())
+    val userView = UserView(Some(userUuid), "james", "james@email.com")
+    when(userViewDaoMock.findAll())
       .thenReturn(Future(Seq(userView)))
-      when(userViewDaoMock.findByEmail(any[String]))
+    when(userViewDaoMock.findByEmail(any[String]))
       .thenReturn(Future(Seq(userView)))
 
-      val Some(response) = route(
-        mockedApp,
-        FakeRequest(Helpers.POST, "/menu/random")
-          .withJsonBody(Json.parse("{}"))
-      )
+    When("request a POST request for random menu")
+    val Some(response) = route(
+      mockedApp,
+      FakeRequest(Helpers.POST, "/menu/random")
+        .withJsonBody(Json.parse("{}"))
+    )
 
-      val responseStatus = status(response)
-      assert(responseStatus == OK)
+    Then("should return status of ok")
+    val responseStatus = status(response)
+    responseStatus should equal(OK)
 
-      val responseContent = contentAsJson(response)
-      val uuid = (responseContent \ "result").as[String]
-      assert(
-        uuid == "123e4567-e89b-12d3-a456-426655440000" ||
-        uuid == "223e4567-e89b-12d3-a456-426655440000"
-      )
-    }
+    And("should return either apple pie or pear pie")
+    val responseContent = contentAsJson(response)
+    val uuid = (responseContent \ "result").as[String]
+    uuid should (equal("123e4567-e89b-12d3-a456-426655440000") or equal("223e4567-e89b-12d3-a456-426655440000"))
   }
 }
