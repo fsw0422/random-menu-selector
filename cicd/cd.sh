bedecb3d6578c64bda4900d21813cb37ff093a39
Kevin Kwon <fsw0422@gmail.com>
2019-04-17 01:47:50 +0200

    Fixed docker image update issue in cd script

--- cicd/cd.sh
+++ cicd/cd.sh
@@ -5,6 +5,7 @@ echo "enter hostname:"
 read host
 ssh kev@${host} <<-EOF
 	git clone https://github.com/fsw0422/random-menu-selector || git -C ~/random-menu-selector pull
+	docker-compose -f ~/random-menu-selector/cicd/docker-compose.yml pull
 	docker-compose -f ~/random-menu-selector/cicd/docker-compose.yml down
 	docker-compose -f ~/random-menu-selector/cicd/docker-compose.yml up -d
 EOF
