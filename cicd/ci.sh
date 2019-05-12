7050f8f591c1ff52642798872c9beb6d445e4d98
Kevin Kwon <fsw0422@gmail.com>
2019-05-12 15:38:31 +0200

    Changed ci to run in Docker

--- cicd/ci.sh
+++ cicd/ci.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-sbt -jvm-debug 5005 \
--DPOSTGRES_PASSWORD=fake \
--DPOSTGRES_HOST=localhost \
--DPOSTGRES_PORT=54320 \
--DPOSTGRES_DB=random_menu_selector \
--DPOSTGRES_SSL_MODE=disable \
--DWRITE_PASSWORD=fake \
--DEMAIL_PASSWORD=fake \
-";clean ;scapegoat ;coverage ;test ;coverageReport"
+# TODO: make test network for testing
+docker run --net host \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v ${PWD}:/workspace/random-menu-selector \
+  -v $HOME/.ivy2:/root/.ivy2 \
+  -e WORKSPACE=${PWD} \
+  fsw0422/random-menu-selector-build:latest \
+  bash -c "cd /workspace/random-menu-selector && \
+		sbt -jvm-debug 5005 \
+			-DPOSTGRES_PASSWORD=fake \
+			-DPOSTGRES_HOST=localhost \
+			-DPOSTGRES_PORT=54320 \
+			-DPOSTGRES_DB=random_menu_selector \
+			-DPOSTGRES_SSL_MODE=disable \
+			-DWRITE_PASSWORD=fake \
+			-DEMAIL_PASSWORD=fake \
+			\";clean ;scapegoat ;coverage ;test ;coverageReport\""
