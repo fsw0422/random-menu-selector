18e9f92ca71df8b54f4605c2beec931a043c45f6
Kevin Kwon <fsw0422@gmail.com>
2019-08-07 19:06:41 +0200

    Change configuration passphrase to be input before ssh due to tty issue in gpg

--- cicd/cd.sh
+++ cicd/cd.sh
@@ -2,9 +2,12 @@
 
 sbt docker:publish
 
+echo Enter configuration unlock passphrase:
+read -s password
+
 ssh kev@fsw0422.com <<-EOF
 	git clone https://github.com/fsw0422/random-menu-selector || git -C ~/random-menu-selector pull
-	gpg --decrypt --pinentry-mode=loopback ~/random-menu-selector/cicd/production.env.gpg > ~/random-menu-selector/cicd/production.env
+	echo $password | gpg --batch --yes --passphrase-fd 0 ~/random-menu-selector/cicd/production.env.gpg
 	docker-compose -f ~/random-menu-selector/cicd/docker-compose.yml pull
 	docker-compose -f ~/random-menu-selector/cicd/docker-compose.yml down
 	docker-compose -f ~/random-menu-selector/cicd/docker-compose.yml up -d
