8c66561ec060ab145e227a120368d81cb4647e68
Kevin Kwon <fsw0422@gmail.com>
2019-08-07 18:36:05 +0200

    Change gpg decryption for env variable to pin entry mode

--- cicd/cd.sh
+++ cicd/cd.sh
@@ -4,7 +4,7 @@ sbt docker:publish
 
 ssh kev@fsw0422.com <<-EOF
 	git clone https://github.com/fsw0422/random-menu-selector || git -C ~/random-menu-selector pull
-  gpg ~/random-menu-selector/cicd/production.env.gpg
+  gpg --decrypt --pinentry-mode=loopback ~/random-menu-selector/cicd/production.env.gpg > ~/random-menu-selector/cicd/production.env
 	docker-compose -f ~/random-menu-selector/cicd/docker-compose.yml pull
 	docker-compose -f ~/random-menu-selector/cicd/docker-compose.yml down
 	docker-compose -f ~/random-menu-selector/cicd/docker-compose.yml up -d
