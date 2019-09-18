8ff362191de559afbe8027bf57c449443b22a970
Kevin Kwon <fsw0422@gmail.com>
2019-09-18 03:34:55 +0200

    Change repo address from HTTP to SSH

--- cicd/cd.sh
+++ cicd/cd.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sbt docker:publish
+#sbt docker:publish
 
 echo Enter configuration unlock passphrase:
 read -s password
 
 ssh root@fsw0422.com <<-EOF
-	git clone https://github.com/fsw0422/random-menu-selector || git -C ~/random-menu-selector pull
+	git clone git@github.com:fsw0422/random-menu-selector.git || git -C ~/random-menu-selector pull
 	echo $password | gpg --batch --yes --passphrase-fd 0 ~/random-menu-selector/cicd/production.env.gpg
 	docker-compose -f ~/random-menu-selector/cicd/docker-compose.yml pull
 	docker-compose -f ~/random-menu-selector/cicd/docker-compose.yml down
