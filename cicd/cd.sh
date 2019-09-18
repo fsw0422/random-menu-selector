e2afcbef202e4963716fdef00d67c33cb44a98d9
Kevin Kwon <fsw0422@gmail.com>
2019-09-18 02:14:48 +0200

    wip

--- cicd/cd.sh
+++ cicd/cd.sh
@@ -5,7 +5,7 @@ sbt docker:publish
 echo Enter configuration unlock passphrase:
 read -s password
 
-ssh kev@fsw0422.com <<-EOF
+ssh root@fsw0422.com <<-EOF
 	git clone https://github.com/fsw0422/random-menu-selector || git -C ~/random-menu-selector pull
 	echo $password | gpg --batch --yes --passphrase-fd 0 ~/random-menu-selector/cicd/production.env.gpg
 	docker-compose -f ~/random-menu-selector/cicd/docker-compose.yml pull
