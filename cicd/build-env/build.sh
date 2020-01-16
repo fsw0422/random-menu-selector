2c8ada1037601d6f2452f634e17e67fa84f23405
Kevin Kwon <fsw0422@gmail.com>
2020-01-16 16:39:03 +0100

    Removed OS specific build environment and added cleanup for mounted data

--- cicd/build-env/build.sh
+++ cicd/build-env/build.sh
@@ -1,28 +1,5 @@
 #!/usr/bin/env bash
 
-echo "======================================"
-echo "Build environment detected [$OSTYPE]"
-echo "======================================"
-
-if [[ "$OSTYPE" == *"linux-gnu"* ]]; then
-	# In Linux, Docker user ids are mapped host to container one-to-one by default
-	# The problem arises when container starts creating files into directories that are mounted to host,
-	# causing the host to have no permission to write to the created files
-	# It is better to create a user in the container that maps to the same userId / groupId to the host
-	docker build \
-		--build-arg USER_NAME=${USER} \
-		--build-arg USER_ID=$(id -u ${USER}) \
-		--build-arg GROUP_ID=$(id -g ${USER}) \
+docker build \
 		-t fsw0422/random_menu_selector/cicd:latest \
 		cicd/build-env
-elif [[ "$OSTYPE" == *"darwin"* ]]; then
-	# Mac users are not running Docker natively and by utilizing transparent virtual machines (HyperKit)
-	# These virtual machines do not have this problem since container to host userId mapping is not the same as Linux
-	# We just use root by default
-	docker build \
-	  -t fsw0422/random_menu_selector/cicd:latest \
-		cicd/build-env
-else
-	echo "Build environment does not support operating systems other than Mac or Linux Distros!"
-	exit 1
-fi
--- cicd/cd.sh
+++ cicd/cd.sh
@@ -1,50 +1,35 @@
 #!/usr/bin/env bash
 
-RUNNING_USER=""
-if [[ "$OSTYPE" == *"linux-gnu"* ]]; then
-	RUNNING_USER=${USER}
-elif [[ "$OSTYPE" == *"darwin"* ]]; then
-	RUNNING_USER="root"
-else
-	echo "Build environment does not support operating systems other than Mac or Linux Distros!"
-	exit 1
-fi
-
-echo ""
-echo "====================================="
-echo "CD environment detected [$OSTYPE]"
-echo "Running container user as ${RUNNING_USER}"
-echo "====================================="
-echo ""
-
-echo ""
-echo "====================================="
-echo "Database evolution commencing"
-echo "====================================="
-echo ""
 echo "Enter configuration unlock passphrase:"
 read -s password
 echo $password | gpg --batch --yes --passphrase-fd 0 ${PWD}/cicd/production.env.gpg
 
 echo ""
 echo "====================================="
-echo "Container image publish commencing"
+echo "Following operations are commencing"
+echo "- Database evolution"
+echo "- Container image publish"
 echo "====================================="
 echo ""
 docker run -it \
 	--env-file ${PWD}/cicd/production.env \
 	-v /var/run/docker.sock:/var/run/docker.sock \
-	-v ${HOME}/.ivy2/cache:/${RUNNING_USER}/.ivy2/cache \
-	-v ${HOME}/.sbt/boot:/${RUNNING_USER}/.sbt/boot \
+	-v ${HOME}/.ivy2/cache:/root/.ivy2/cache \
+	-v ${HOME}/.sbt/boot:/root/.sbt/boot \
 	-v ${PWD}:/random_menu_selector \
 	fsw0422/random_menu_selector/cicd:latest \
-	bash -c "docker login && cd /random_menu_selector && db/evolve.sh && sbt docker:publish"
+	bash -c "docker login && cd /random_menu_selector && db/evolve.sh && sbt docker:publish && rm -rf target"
 
 if [ $? -eq 0 ]
 then
 	echo ""
 	echo "====================================="
-	echo "Container image publish has succeeded"
+  echo "Following operations have succeeded"
+	echo "- Database evolution"
+	echo "- Container image publish"
+	echo "====================================="
+	echo ""
+	echo "====================================="
 	echo "Deployment Commencing"
 	echo "====================================="
 	echo ""
