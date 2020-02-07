a9266a9f9d46fb95d8ada3b97fc2d02e85a162be
Kevin Kwon <fsw0422@gmail.com>
2020-02-07 16:22:43 +0100

    wip

--- cicd/ci.sh
+++ cicd/ci.sh
@@ -3,12 +3,6 @@
 export POSTGRES_DB=random_menu_selector # Always sync this value with PROD value due to integration test (see production.env.gpg)
 export POSTGRES_USER=postgres # Always sync this value with PROD value due to integration test (see production.env.gpg)
 export POSTGRES_PASSWORD=fake
-export POSTGRES_HOST=localhost
-export POSTGRES_PORT=5432
-export POSTGRES_SSL_MODE=disable
-export WRITE_PASSWORD=fake
-export EMAIL_USER=fake@mail.com
-export EMAIL_PASSWORD=fake
 
 source venv/bin/activate
 cicd/test-runner.py
--- cicd/test-runner.py
+++ cicd/test-runner.py
@@ -24,27 +24,39 @@ print("Running container user as " + USER)
 print("Test commencing")
 print("=========================================")
 
+os.system("printf 'y\n' | docker network prune")
 os.system("printf 'y\n' | docker volume prune")
-db_container = PostgresContainer("postgres:9.6")
+os.system("docker network create random_menu_selector_network")
+
+db_container = PostgresContainer("postgres:9.6").with_name("random_menu_selector_db")
 with db_container as db:
+    os.system("docker network connect random_menu_selector_network random_menu_selector_db")
     os.system(
         "docker run" +
-        " --network=host"
+        " --network=random_menu_selector_network" +
+        # TODO: make it optionable
+        ["", " -p 5005:5005"][True] +
         " -u " + USER +
-        " -w " + USER_HOME +
+        " -w " + USER_HOME + "/random_menu_selector"
         " -v /var/run/docker.sock:/var/run/docker.sock" +
         " -v ${HOME}/.ivy2:" + USER_HOME + "/.ivy2" +
         " -v ${HOME}/.sbt/boot:" + USER_HOME + "/.sbt/boot" +
         " -v ${PWD}:" + USER_HOME + "/random_menu_selector" +
-        " -e POSTGRES_HOST=" + os.environ["POSTGRES_HOST"] +
-        " -e POSTGRES_PORT=" + db.get_exposed_port(os.environ["POSTGRES_PORT"]) + # Map port to the randomized port
-        " -e POSTGRES_SSL_MODE=" + os.environ["POSTGRES_SSL_MODE"] +
+        " -e POSTGRES_HOST=random_menu_selector_db"
+        " -e POSTGRES_PORT=5432"
+        " -e POSTGRES_SSL_MODE=disable"
         " -e POSTGRES_DB=" + os.environ["POSTGRES_DB"] +
         " -e POSTGRES_USER=" + os.environ["POSTGRES_USER"] +
         " -e POSTGRES_PASSWORD=" + os.environ["POSTGRES_PASSWORD"] +
-        " -e WRITE_PASSWORD=" + os.environ["WRITE_PASSWORD"] +
-        " -e EMAIL_USER=" + os.environ["EMAIL_USER"] +
-        " -e EMAIL_PASSWORD=" + os.environ["EMAIL_PASSWORD"] +
+        " -e WRITE_PASSWORD=fake"
+        " -e EMAIL_USER=fake@mail.com"
+        " -e EMAIL_PASSWORD=fake"
         " fsw0422/random_menu_selector/cicd:latest" +
-        " bash -c \"cd " + USER_HOME + "/random_menu_selector && db/evolve.sh && sbt \\\";clean ;test\\\"\""
+        " bash -c \""
+        " db/evolve.sh &&"
+        " sbt" +
+        # TODO: make it optionable
+        ["", " -jvm-debug 5005"][True] +
+        " \\\";clean ;test\\\""
+        "\""
     )
