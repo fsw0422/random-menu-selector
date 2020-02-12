71524f6ec8b72bf6a6919d594df40bb29eebfb42
Kevin Kwon <fsw0422@gmail.com>
2020-02-12 19:54:15 +0100

    wip

--- cicd/test-runner.py
+++ cicd/test-runner.py
@@ -45,40 +45,41 @@ print("Running container user as " + USER)
 print("Test commencing")
 print("=========================================")
 
-os.system("docker rm -f random_menu_selector_build")
-os.system("docker rm -f random_menu_selector_db")
-os.system("printf 'y\n' | docker network prune")
-os.system("printf 'y\n' | docker volume prune")
 
-os.system("docker network create random_menu_selector_network")
-db_container = PostgresContainer("postgres:9.6").with_name("random_menu_selector_db")
-with db_container as db:
-    os.system("docker network connect random_menu_selector_network random_menu_selector_db")
-    os.system(
-        "docker run" +
-        " --name random_menu_selector_build"
-        " --network=random_menu_selector_network" +
-        ["", " -p 5005:5005"][args.d] +
-        " -u " + USER +
-        " -w " + USER_HOME + "/random_menu_selector"
-        " -v /var/run/docker.sock:/var/run/docker.sock" +
-        " -v ${HOME}/.ivy2:" + USER_HOME + "/.ivy2" +
-        " -v ${HOME}/.sbt/boot:" + USER_HOME + "/.sbt/boot" +
-        " -v ${PWD}:" + USER_HOME + "/random_menu_selector" +
-        " -e POSTGRES_HOST=random_menu_selector_db"
-        " -e POSTGRES_PORT=5432"
-        " -e POSTGRES_SSL_MODE=disable"
-        " -e POSTGRES_DB=" + os.environ["POSTGRES_DB"] +
-        " -e POSTGRES_USER=" + os.environ["POSTGRES_USER"] +
-        " -e POSTGRES_PASSWORD=" + os.environ["POSTGRES_PASSWORD"] +
-        " -e WRITE_PASSWORD=fake"
-        " -e EMAIL_USER=fake@mail.com"
-        " -e EMAIL_PASSWORD=fake"
-        " fsw0422/random_menu_selector/cicd:latest" +
-        " bash -c \""
-        " db/evolve.sh &&"
-        " sbt" +
-        ["", " -jvm-debug 5005"][args.d] +
-        " \\\";clean ;test\\\""
-        "\""
-    )
+try:
+    with PostgresContainer("postgres:9.6").with_name("random_menu_selector_db") as db:
+        os.system("docker network create random_menu_selector_network")
+        os.system("docker network connect random_menu_selector_network random_menu_selector_db")
+        os.system(
+            "docker run" +
+            " --name random_menu_selector_build"
+            " --network=random_menu_selector_network" +
+            ["", " -p 5005:5005"][args.d] +
+            " -u " + USER +
+            " -w " + USER_HOME + "/random_menu_selector"
+            " -v /var/run/docker.sock:/var/run/docker.sock" +
+            " -v ${HOME}/.ivy2:" + USER_HOME + "/.ivy2" +
+            " -v ${HOME}/.sbt/boot:" + USER_HOME + "/.sbt/boot" +
+            " -v ${PWD}:" + USER_HOME + "/random_menu_selector" +
+            " -e POSTGRES_HOST=random_menu_selector_db"
+            " -e POSTGRES_PORT=5432"
+            " -e POSTGRES_SSL_MODE=disable"
+            " -e POSTGRES_DB=" + os.environ["POSTGRES_DB"] +
+            " -e POSTGRES_USER=" + os.environ["POSTGRES_USER"] +
+            " -e POSTGRES_PASSWORD=" + os.environ["POSTGRES_PASSWORD"] +
+            " -e WRITE_PASSWORD=fake"
+            " -e EMAIL_USER=fake@mail.com"
+            " -e EMAIL_PASSWORD=fake"
+            " fsw0422/random_menu_selector/cicd:latest" +
+            " bash -c \""
+            " db/evolve.sh &&"
+            " sbt" +
+            ["", " -jvm-debug 5005"][args.d] +
+            " \\\";clean ;test\\\""
+            "\""
+        )
+finally:
+    os.system("docker rm -f random_menu_selector_build")
+    os.system("docker rm -f random_menu_selector_db")
+    os.system("printf 'y\n' | docker network prune")
+    os.system("printf 'y\n' | docker volume prune")
