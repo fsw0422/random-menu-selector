95d0c936575d9d947195b49063e49daa391dcb5d
Kevin Kwon <fsw0422@gmail.com>
2020-06-16 14:15:39 +0200

    Added restart logic

--- scheduler/cd.sh
+++ scheduler/cd.sh
@@ -9,6 +9,8 @@ ssh kev@fsw0422.com <<-EOF
 	git clone git@github.com:fsw0422/random-menu-selector.git || git -C ~/random-menu-selector pull
 
   cd random-menu-selector/scheduler
-  docker run -d fsw0422/random_menu_selector_menu_scheduler:latest
+  docker stop random_menu_selector_menu_scheduler
+  docker rm random_menu_selector_menu_scheduler
+  docker run -d --name random_menu_selector_menu_scheduler fsw0422/random_menu_selector_menu_scheduler:latest
 EOF
 
