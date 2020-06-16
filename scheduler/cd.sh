18f5eb583d0677fb527e3de79b46be9e64611e17
Kevin Kwon <fsw0422@gmail.com>
2020-06-16 14:12:21 +0200

    Changed to Docker daemon mode

--- scheduler/cd.sh
+++ scheduler/cd.sh
@@ -9,6 +9,6 @@ ssh kev@fsw0422.com <<-EOF
 	git clone git@github.com:fsw0422/random-menu-selector.git || git -C ~/random-menu-selector pull
 
   cd random-menu-selector/scheduler
-  docker run fsw0422/random_menu_selector_menu_scheduler:latest
+  docker run -d fsw0422/random_menu_selector_menu_scheduler:latest
 EOF
 
